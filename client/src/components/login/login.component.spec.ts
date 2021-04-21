2821d16c62385e881c9a9920cb776cbb16b3a94b
mf589078 <mf589078@fh-muenster.de>
2021-04-20 23:45:19 -0700

    Teilkomponenten von Login

---
+++ client/src/components/login/login.component.spec.ts
@@ -0,0 +1,25 @@
+/* Autor: Martin Feldman */
+
+
+import { LitElement } from 'lit-element';
+import './sign-in.component';
+
+
+describe('login', () => {
+  let element: LitElement;
+
+  beforeEach(async () => {
+    element = document.createElement('app-sign-in') as LitElement;
+    document.body.appendChild(element);
+    await element.updateComplete;
+  });
+
+  afterEach(() => {
+    element.remove();
+  });
+
+  it(' "Anmelden"', async () => {
+    const h1Elem = element.shadowRoot!.querySelector('h1') as HTMLElement;
+    expect(h1Elem.innerText).toBe('Anmelden');
+  });
+});
