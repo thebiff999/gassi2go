7c971532f246aaf69aebde577b2e1b244751daae
mf589078 <mf589078@fh-muenster.de>
2021-04-20 23:47:02 -0700

    added login components

---
+++ client/src/components/login/login.components.ts
@@ -0,0 +1,48 @@
+/* Autor: Martin Feldman */
+
+import { css, customElement, html, LitElement, query, unsafeCSS } from "lit-element";
+import { httpClient } from '../../http-client';
+import { router } from '../../router';
+import { PageMixin } from '../page.mixin';
+
+const componentCSS = require('./login.component.scss');
+
+//Bezug zu CSS und shared CSS fehlt noch
+
+class LoginComponent extends PageMixin(LitElement) {
+
+
+@query('form')
+form!: HTMLFormElement;
+
+@query('#email')
+emailElement!: HTMLInputElement;
+
+@query('#password')
+passwordElement!: HTMLInputElement;
+
+render() {
+  return html`
+    ${this.renderNotification()}
+    <h1>Anmelden</h1>
+    <form>
+      <div class="form-group">
+        <label class="control-label" for="email">E-Mail</label>
+        <input class="form-control" type="email" autofocus required id="email" name="email" />
+        <div class="invalid-feedback">Eine gültige E-Mailadresse ist für die Anmeldung notwendig</div>
+      </div>
+      <div class="form-group">
+        <label class="control-label" for="password">Passwort</label>
+        <input class="form-control" type="password" required id="password" name="password" />
+        <div class="invalid-feedback">Die Vergabe eines Passwortes ist zwingend</div>
+      </div>
+      <button class="btn btn-primary" type="button" @click="${this.submit}">Login</button>
+    </form>
+  `;
+}
+
+
+}
+
+}
+}
