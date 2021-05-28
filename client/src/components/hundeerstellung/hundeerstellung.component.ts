7b5ba22212fd051f8388ecc271ce4e923891785e
Simon Flathmann <sf402517@fh-muenster.de>
2021-05-28 16:04:43 +0200

    added picture upload in hundeerstellung

--- client/src/components/hundeerstellung/hundeerstellung.component.ts
+++ client/src/components/hundeerstellung/hundeerstellung.component.ts
@@ -35,19 +35,29 @@ class HundeerstellungComponent extends LitElement{
                                     <label for="geb">Geburtsdatum</label>
                                     <input type="date" class="form-control" id="geb">
                                 </div>
-                                <div class="form-group col-md-12">
-                                    <label for="info">zusätzliche Informationen</label>
-                                    <textarea class="form-control" id="info" rows="5" minlength="30" maxlength="1000" required
-                                        placeholder="Hier können Sie ihren Hund beschreiben."></textarea>
-                                </div>
                             </div>
-
                         </div>
+<!--                         <div class="col-lg-4">
+                            <div class="picture m-auto">
+                                <img id="hunde-image" alt="placeholder dog image" src="./../../../../resources/images/dog_logo.png">
+                            </div> 
+                            <button class="btn btn-sm btn-white d-table mx-auto mt-4" type="button" @click="${this.upload}">Foto hochladen</button>
+                        </div> -->
                         <div class="col-lg-4">
                             <div class="picture m-auto">
-                                <img id="hunde-image" src="./../../../../resources/images/dog_logo.png">
-                            </div> 
-                            <button class="btn btn-sm btn-white d-table mx-auto mt-4">Foto hochladen</button>
+                                <img src="./../../../../resources/images/dog_logo.png" id="hunde-image">
+                            </div>
+                            <div class="pictureinput m-4">
+                                <input type="file" id="file" hidden>
+                                <button id="uploadbtn" class="btn btn-sm btn-white d-table mx-auto mt-4" type="button">Foto hochladen</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row m-4">
+                        <div class="form-group col-md-12">
+                                    <label for="info">Zusätzliche Informationen</label>
+                                    <textarea class="form-control" id="info" rows="5" minlength="30" maxlength="1000" required
+                                        placeholder="Hier können Sie ihren Hund beschreiben."></textarea>
                         </div>
                     </div>
                     <div class="form-row">
@@ -60,8 +70,40 @@ class HundeerstellungComponent extends LitElement{
         `
     }
 
+    //Hinzufügen eines Event-Listeners, der das unsichtbare Input-Element für den Datenupload aufruft.
+    updated(){
+        var component = this.shadowRoot!;
+        var uploadbtn = component!.getElementById('uploadbtn');
+        var file = component!.getElementById('file');
+        var img = component!.getElementById('hunde-image');
+
+        uploadbtn!.addEventListener('click', openDialog);
+        function openDialog(){
+            console.log('opened Dialog');
+            component!.getElementById('file')!.click();
+        }
+
+        file!.addEventListener('change', function(){
+            let file2 = file! as HTMLInputElement;
+            let uploadedFile = file2.files![0];
+
+            if(uploadedFile){
+                const reader = new FileReader();
+                reader.addEventListener('load', function(){
+                    if(typeof reader.result === 'string' )
+                    img!.setAttribute('src', reader.result);
+                });
+                reader.readAsDataURL(uploadedFile);
+            }
+        })
+    }
+
     async create() {
         console.log("erstellung");
+    }
+
+    upload(){
+        console.log("upload");
     }
 
 }
