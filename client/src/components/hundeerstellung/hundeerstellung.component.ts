9edb12b3f8e8f4533980f330c42b4c5e9ec08010
Simon Flathmann <sf402517@fh-muenster.de>
2021-05-28 18:01:25 +0200

    added validation for hundeerstellung

--- client/src/components/hundeerstellung/hundeerstellung.component.ts
+++ client/src/components/hundeerstellung/hundeerstellung.component.ts
@@ -1,4 +1,4 @@
-import { css, customElement, html, LitElement, unsafeCSS } from "lit-element";
+import { css, customElement, html, LitElement, query, unsafeCSS } from "lit-element";
 
 const hundeerstellungComponentSCSS = require('./hundeerstellung.component.scss');
 
@@ -13,6 +13,24 @@ class HundeerstellungComponent extends LitElement{
         super();
     }
 
+    @query('form')
+    form!: HTMLFormElement;
+
+    @query('#name')
+    name!: HTMLInputElement;
+
+    @query('#rasse')
+    rasse!: HTMLInputElement;
+
+    @query('#geb')
+    geb!: HTMLInputElement;
+
+    @query('#info')
+    info!: HTMLTextAreaElement;
+
+    @query('#file')
+    file!: HTMLInputElement;
+
     render(){
         return html`
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
@@ -25,31 +43,29 @@ class HundeerstellungComponent extends LitElement{
                             <div class="form-row">
                                 <div class="form-group col-md-12">
                                     <label for="name">Name</label>
-                                    <input type="text" class="form-control" id="name" placeholder="Max">
+                                    <input type="text" class="form-control" id="name" placeholder="Max" required>
+                                    <div class="invalid-feedback">Der Name ist erforderlich.</div>
                                 </div>
                                 <div class="form-group col-md-12">
                                     <label for="rasse">Rasse</label>
-                                    <input type="text" class="form-control" id="rasse" placeholder="Bernasenne">
+                                    <input type="text" class="form-control" id="rasse" placeholder="Bernasenne" required>
+                                    <div class="invalid-feedback">Die Rasse ist erforderlich.</div>
                                 </div>
                                 <div class="form-group col-md-12">
                                     <label for="geb">Geburtsdatum</label>
-                                    <input type="date" class="form-control" id="geb">
+                                    <input type="date" class="form-control" id="geb" required>
+                                    <div class="invalid-feedback">Bitte wähle ein Datum in der Vergangenheit.</div>
                                 </div>
                             </div>
                         </div>
-<!--                         <div class="col-lg-4">
-                            <div class="picture m-auto">
-                                <img id="hunde-image" alt="placeholder dog image" src="./../../../../resources/images/dog_logo.png">
-                            </div> 
-                            <button class="btn btn-sm btn-white d-table mx-auto mt-4" type="button" @click="${this.upload}">Foto hochladen</button>
-                        </div> -->
                         <div class="col-lg-4">
                             <div class="picture m-auto">
                                 <img src="./../../../../resources/images/dog_logo.png" id="hunde-image">
                             </div>
                             <div class="pictureinput m-4">
-                                <input type="file" id="file" hidden>
+                                <input type="file" id="file" class="form-control" hidden>
                                 <button id="uploadbtn" class="btn btn-sm btn-white d-table mx-auto mt-4" type="button">Foto hochladen</button>
+                                <div class="invalid-feedback">Der Datentyp ist nicht valide. Die erlaubten Datentypen sind: jpg, jpeg und png.</div>
                             </div>
                         </div>
                     </div>
@@ -58,6 +74,7 @@ class HundeerstellungComponent extends LitElement{
                                     <label for="info">Zusätzliche Informationen</label>
                                     <textarea class="form-control" id="info" rows="5" minlength="30" maxlength="1000" required
                                         placeholder="Hier können Sie ihren Hund beschreiben."></textarea>
+                                    <div class="invalid-feedback">Die Zusätzlichen Informationen sind erforderlich und müssen zwischen 30 und 1000 Zeichen lang sein.</div>
                         </div>
                     </div>
                     <div class="form-row">
@@ -70,8 +87,72 @@ class HundeerstellungComponent extends LitElement{
         `
     }
 
-    //Hinzufügen eines Event-Listeners, der das unsichtbare Input-Element für den Datenupload aufruft.
+    //Wird im Lit-Lebenszyklus nach dem Rendern aufgerufen
     updated(){
+        this.setMaxDate();
+        this.initPictureUpload();
+    }
+
+    async create() {
+        console.log("erstellung");
+        if(this.checkInputs()){
+            console.log("checked successfully")
+        }
+        else{
+            this.form.classList.add('was-validated');
+        }
+    }
+    
+    //Validierung der eingegebenen Daten
+    checkInputs(){
+        console.log('checkingInputs');
+        var file = this.file;
+        var allowedExtensions = /(\.jpg|\.jpeg|\.png)$/i;
+        console.log(this.file);
+        console.log(this.file.value);
+
+        //Falls eine Datei hochgeladen wurde, wird diese auf den Datentypen überprüft
+        if(this.file.value !== ""){
+            if(!allowedExtensions.exec(this.file.value))
+            {
+                this.file.setCustomValidity("Der Datentyp ist nicht valide. Die erlaubten Datentypen sind: jpg, jpeg und png.")
+            }
+            else{
+                this.file.setCustomValidity('');
+            }
+        }
+
+
+        return this.form.checkValidity();
+    }
+
+    /* Setzt für das Input-Elements vom Geburtstag das max-Date auf den aktuellen Tag */
+    setMaxDate(){
+        var today = new Date();
+        var ddNum = today.getDate();
+        var dd = ddNum.toString();
+
+        var mmNum = today.getMonth() + 1;
+        var mm = mmNum.toString();
+
+        var yyyyNum = today.getFullYear();
+        var yyyy = yyyyNum.toString();
+
+        if(ddNum < 10){
+            dd = '0' + dd;
+        }
+        if(mmNum < 10){
+            mm = '0' + mm;
+        }
+
+        var todayStr = yyyy+"-"+mm+"-"+dd;
+        console.log("Attribut max vom Geburtsdatum wird auf " + todayStr + " gesetzt.");
+        this.shadowRoot!.getElementById('geb')?.setAttribute('max', todayStr);
+    }
+
+    /* Setzt einen EventListener für den Upload-Button, welches das unsichtbare input-Element aufruft. 
+        Diesem wird ein auch ein EventListener hinzugefügt, welcher beim Datenupload das src-Attribut vom img-Element ändert. */
+    initPictureUpload(){
         var component = this.shadowRoot!;
         var uploadbtn = component!.getElementById('uploadbtn');
         var file = component!.getElementById('file');
@@ -88,6 +169,7 @@ class HundeerstellungComponent extends LitElement{
             let uploadedFile = file2.files![0];
 
             if(uploadedFile){
+                //Preview
                 const reader = new FileReader();
                 reader.addEventListener('load', function(){
                     if(typeof reader.result === 'string' )
@@ -96,14 +178,5 @@ class HundeerstellungComponent extends LitElement{
                 reader.readAsDataURL(uploadedFile);
             }
         })
-    }
-
-    async create() {
-        console.log("erstellung");
-    }
-
-    upload(){
-        console.log("upload");
-    }
-
+    };
 }
